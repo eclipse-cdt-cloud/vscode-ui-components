c0d16d9ef3aa69b221bc061da5fc351821457f71
Haydar Metin <hmetin@eclipsesource.com>
2025-03-31 08:53:56 +0200

    Migrate editing, polling and resizing functionality (#2)
    * Migrate editing, polling and resizing functionality
    
    * PR Feedback

--- src/label/label-helpers.tsx
+++ src/label/label-helpers.tsx
@@ -1,5 +1,5 @@
 /**********************************************************************************
- * Copyright (c) 2025 Company and others.
+ * Copyright (c) 2025 EclipseSource and others.
  *
  * This program and the accompanying materials are made available under the
  * terms of the MIT License as outlined in the LICENSE file.
@@ -8,7 +8,7 @@
 import React from 'react';
 import Markdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
-import { Tooltip, TooltipTrigger, TooltipContent } from '../tooltip/tooltip';
+import { Tooltip, TooltipContent, TooltipTrigger } from '../tooltip/tooltip';
 
 export function createHighlightedText(label?: string, highlights?: [number, number][]): React.JSX.Element {
     if (label === undefined) {
@@ -48,7 +48,7 @@ export function createHighlightedText(label?: string, highlights?: [number, numb
 }
 
 export function createLabelWithTooltip(child: React.JSX.Element, tooltip?: string): React.JSX.Element {
-    const label = <div className='tree-label flex-auto flex align-items-center'>{child}</div>;
+    const label = <div className='tree-label'>{child}</div>;
 
     if (tooltip === undefined) {
         return label;
--- src/tree/common/tree-messenger-types.ts
+++ src/tree/common/tree-messenger-types.ts
@@ -8,15 +8,7 @@
 import type { NotificationType } from 'vscode-messenger-common';
 import type { CDTTreeExtensionModel } from './tree-model-types';
 
-export interface CDTTreeNotificationContext {
-    /**
-     * If true or undefined, the tree will be resynced.
-     */
-    resync?: boolean;
-}
-
 export interface CDTTreeNotification<T> {
-    context?: CDTTreeNotificationContext;
     data: T;
 }
 
@@ -34,8 +26,19 @@ export interface CDTTreeExecuteCommand {
     value?: unknown;
 }
 
+export interface CDTTreePartialUpdate<TItem = unknown> {
+    items?: TItem[];
+}
+
 export namespace CDTTreeMessengerType {
+    /**
+     * Replace the current state with the given state.
+     */
     export const updateState: NotificationType<CDTTreeExtensionModel> = { method: 'updateState' };
+    /**
+     * Update the nodes with the given nodes.
+     */
+    export const updatePartial: NotificationType<CDTTreePartialUpdate> = { method: 'updatePartial' };
     export const ready: NotificationType<void> = { method: 'ready' };
 
     export const executeCommand: NotificationType<CDTTreeNotification<CDTTreeExecuteCommand>> = { method: 'executeCommand' };
